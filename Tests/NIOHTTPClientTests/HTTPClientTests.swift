a6309b1280e5fb06567131b3156d33219cdec25e
vkill <vkill.net@gmail.com>
2019-02-21 16:06:30 +0800

    Update

--- Tests/NIOHTTPClientTests/HTTPClientTests.swift
+++ Tests/NIOHTTPClientTests/HTTPClientTests.swift
@@ -31,32 +31,46 @@ final class HTTPClientTests: XCTestCase {
     
     func testServerNonTLSWithNonProxy() throws {
         let clientHTTPBin = try HTTPClient.start(config: .init(server: serverNonTLSHTTPBin, proxy: nil)).wait()
-        XCTAssertEqual(try clientHTTPBin.get(uri: "http://httpbin.org/status/200").wait().status, .ok)
+        let res = try clientHTTPBin.get(uri: "http://httpbin.org/anything").wait()
+        XCTAssertEqual(res.status, .ok)
     }
     
     func testServerNonTLSWithProxyNonTLS() throws {
         let clientHTTPBin = try HTTPClient.start(config: .init(server: serverNonTLSHTTPBin, proxy: proxyNonTLS)).wait()
-        XCTAssertEqual(try clientHTTPBin.get(uri: "http://httpbin.org/status/200").wait().status, .ok)
+        let res = try clientHTTPBin.get(uri: "http://httpbin.org/anything").wait()
+        XCTAssertEqual(res.status, .ok)
+        guard let body = res.body, case let .whole(data) = body else {
+            fatalError()
+        }
+        print(String(decoding: data, as: UTF8.self))
     }
     
     func testServerNonTLSWithProxyTLS() throws {
         let clientHTTPBin = try HTTPClient.start(config: .init(server: serverNonTLSHTTPBin, proxy: proxyTLS)).wait()
-        XCTAssertEqual(try clientHTTPBin.get(uri: "http://httpbin.org/status/200").wait().status, .ok)
+        let res = try clientHTTPBin.get(uri: "http://httpbin.org/anything").wait()
+        XCTAssertEqual(res.status, .ok)
     }
     
     func testServerTLSWithNonProxy() throws {
         let clientHTTPBin = try HTTPClient.start(config: .init(server: serverTLSHTTPBin, proxy: nil)).wait()
-        XCTAssertEqual(try clientHTTPBin.get(uri: "https://httpbin.org/status/200").wait().status, .ok)
+        let res = try clientHTTPBin.get(uri: "https://httpbin.org/anything").wait()
+        XCTAssertEqual(res.status, .ok)
     }
     
     func testServerTLSWithProxyNonTLS() throws {
         let clientHTTPBin = try HTTPClient.start(config: .init(server: serverTLSHTTPBin, proxy: proxyNonTLS)).wait()
-        XCTAssertEqual(try clientHTTPBin.get(uri: "https://httpbin.org/status/200").wait().status, .ok)
+        let res = try clientHTTPBin.get(uri: "https://httpbin.org/anything").wait()
+        XCTAssertEqual(res.status, .ok)
+        guard let body = res.body, case let .whole(data) = body else {
+            fatalError()
+        }
+        print(String(decoding: data, as: UTF8.self))
     }
     
     func testServerTLSWithProxyTLS() throws {
         let clientHTTPBin = try HTTPClient.start(config: .init(server: serverTLSHTTPBin, proxy: proxyTLS)).wait()
-        XCTAssertEqual(try clientHTTPBin.get(uri: "https://httpbin.org/status/200").wait().status, .ok)
+        let res = try clientHTTPBin.get(uri: "https://httpbin.org/anything").wait()
+        XCTAssertEqual(res.status, .ok)
     }
     
     static var allTests = [
