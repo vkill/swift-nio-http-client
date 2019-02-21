88fbd5467c9d95aa2268bfeaf0928c89594a35fe
vkill <vkill.net@gmail.com>
2019-02-21 17:51:58 +0800

    Update

--- Sources/NIOHTTPClient/HTTPClientRequestEncoder.swift
+++ Sources/NIOHTTPClient/HTTPClientRequestEncoder.swift
@@ -23,9 +23,9 @@ internal final class HTTPClientRequestEncoder: ChannelOutboundHandler {
         
         head.headers.replaceOrAdd(name: "User-Agent", value: "NIOHTTPClient")
         
-        guard var urlComponents = URLComponents(string: head.uri) else {
-            assert(false, "invalid head.uri \(head.uri)")
-        }
+        let urlComponentsTmp = URLComponents(string: head.uri)
+        assert(urlComponentsTmp != nil, "invalid head.uri \(head.uri)")
+        var urlComponents = urlComponentsTmp!
         if let _ = config.proxy {
             if case .https(_) = config.server.scheme {
                 urlComponents.host = config.server.address
@@ -35,9 +35,8 @@ internal final class HTTPClientRequestEncoder: ChannelOutboundHandler {
         if !urlComponents.path.hasPrefix("/") {
             urlComponents.path = "/" + urlComponents.path
         }
-        guard let url = urlComponents.url else {
-            assert(false, "convert URLComponents to URL failed")
-        }
+        assert(urlComponents.url != nil, "convert URLComponents to URL failed")
+        let url = urlComponents.url!
         head.uri = url.absoluteString
         
         ctx.write(wrapOutboundOut(.head(head)), promise: nil)
